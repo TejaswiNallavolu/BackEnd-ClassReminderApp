cd6dd3dd01915e7e81c5555490fd034f1ff2ee20
Tejaswi Nallavolu <tejaswireddynallavolu@gmail.com>
2021-12-03 20:01:31 -0600

    Initial commit

---
+++ AuthProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using AuthProject.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthProject.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<ListOfClasses> ListOfClasses { get; set; }
+    }
+}
---
+++ AuthProject/Models/ListOfClasses.cs
@@ -0,0 +1,13 @@
+﻿namespace AuthProject.Models
+{
+    public class ListOfClasses
+    {
+        public int ID { get; set; }
+        public string ClassName { get; set; }
+        public string Image { get; set; }
+        public string Time { get; set; }
+        public string Location { get; set; }
+
+        public string Link { get; set; }
+    }
+}
