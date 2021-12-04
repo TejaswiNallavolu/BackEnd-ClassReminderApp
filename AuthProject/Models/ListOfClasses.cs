7a021c8a9f5b7c5aba4b7fd07378dfde23304788
Tejaswi Nallavolu <tejaswireddynallavolu@gmail.com>
2021-12-03 20:16:53 -0600

    removed spacing

--- AuthProject/Models/ListOfClasses.cs
+++ AuthProject/Models/ListOfClasses.cs
@@ -7,7 +7,6 @@
         public string Image { get; set; }
         public string Time { get; set; }
         public string Location { get; set; }
-
         public string Link { get; set; }
     }
 }
